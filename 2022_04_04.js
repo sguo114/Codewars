dca3d219923c2bf0e7ffdad62b1b2f8b326069fa
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-04-10 08:39:23 -0400

    Update 2022_04_04.js

--- 2022_04_04.js
+++ 2022_04_04.js
@@ -1,3 +1,5 @@
+// 8kata completed
+
 // 6kyu: Duplicate Encoder 
 function duplicateEncode(word){
     // ...
@@ -36,3 +38,62 @@ function duplicateEncode(word){
 //   console.log(word)
 //   return word
 //}
+
+// 8kyu: Enumerable Magic #20- Cascading Subsets
+function eachCons(array, n) {
+  let x = array.length;
+  let z = []
+  for(let i = 0; i<=x-n; i++){
+    let y = []
+    for(let j =i; j<n+i; j++){
+      y.push(array[j])
+    }
+    z.push(y)
+  }
+  return z;
+}
+
+// 8kyu: Lario and Muigi Pipe Problem:
+function pipeFix(numbers){
+  let start = numbers[0]
+  let end = numbers[numbers.length-1]
+  let ans = []
+  for(let i =start; i<=end; i++){
+    ans.push(i)
+  }
+  return ans
+}
+
+// 8kyu: Freudian translator
+function toFreud(string) {
+let result = ""
+let length = string.split(" ").length 
+  for(let i = 0; i<length; i++) {
+    result += string ==""? "" : i<length-1?'sex ':'sex'
+  }
+  return result
+}
+
+// 8kyu: Beginner- Lost Without a Map
+function maps(x){
+  return x.map(value=>value*2)
+}
+
+// 8kyu: Will you make it?
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump <= mpg*fuelLeft 
+};
+
+// 8kyu: Aspect Ratio Cropping-Part 1
+function aspectRatio(x,y){
+  return [Math.ceil(y*16/9),y] 
+}
+
+// 8kyu: Rock Paper Scissors!
+const rps = (p1, p2) => {
+  if(p1 === 'scissors' && p2 ==='paper' || p1 === 'paper'&& p2==='rock'||p1 ==='rock'&& p2==='scissors'){
+    return 'Player 1 won!'
+  } else if(p1 === p2){
+    return 'Draw!'
+  } else{return 'Player 2 won!'}
+};
