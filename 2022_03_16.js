0243fb8f0b813719642f4834360410ad086c1214
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-04-10 08:09:26 -0400

    Create 2022_03_16.js

---
+++ 2022_03_16.js
@@ -0,0 +1,16 @@
+// 8kyu: Multiply
+function multiply(a, b){
+  a * b
+  return a*b
+}
+
+// 8kyu: Powers of 2
+function powersOfTwo(n){
+  let array = []
+  for (i = 0; i<=n; i++){
+    array.push(2**i);
+  }
+  return array
+  console.log(array)
+}
+powersOfTwo(0)
