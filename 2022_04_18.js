df2670074bb1567de295db72aad63fd416ab9c8b
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-04-18 11:27:37 -0400

    Update 2022_04_18.js

--- 2022_04_18.js
+++ 2022_04_18.js
@@ -1,4 +1,42 @@
-// 5 kata completed
+// 6 kata completed
+
+// 6kyu: If you can read this...
+function to_nato(words) {
+	let NATO = {a: 'Alfa', 
+              b:'Bravo',
+              c:'Charlie',
+              d:'Delta',
+              e:'Echo',
+              f:'Foxtrot', 
+              g:'Golf', 
+              h:'Hotel', 
+              i:'India', 
+              j:'Juliett', 
+              k:'Kilo', 
+              l:'Lima',
+              m:'Mike',
+              n:'November',
+              o:'Oscar',
+              p:'Papa',
+              q:'Quebec',
+              r:'Romeo',
+              s:'Sierra',
+              t:'Tango',
+              u:'Uniform',
+              v:'Victor',
+              w:'Whiskey',
+              x:'Xray',
+              y:'Yankee',
+              z:'Zulu'}
+  let array = words.split('').filter(x=>x!=' ').map(x=>x.toLowerCase())
+  console.log(words,array)
+  for(let i = 0;i<array.length;i++){
+    if(NATO[array[i]] != undefined){
+      array[i] = NATO[array[i]]
+    } else{array[i]}
+  }
+  return array.join(' ')
+}
 
 // 7kyu: You Got Change?
 function giveChange(amount) {
