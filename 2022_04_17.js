dee0a68dae20a97cbc16224aa9e1496663a025ec
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-04-18 10:00:45 -0400

    Update 2022_04_17.js

--- 2022_04_17.js
+++ 2022_04_17.js
@@ -1,4 +1,22 @@
-// 8kyu: Remove First and Last Character
-function removeChar(str){
-  return str.length>1?str.slice(1,str.length-1):str
-};
+// 8kyu: Count of positive/ sum of negatives
+function countPositivesSumNegatives(input) {  
+  let [count,sum] = [0,0]
+  if(input == null||input.length==0){
+    return []
+  } else{
+    for(let i = 0; i<input.length;i++){
+    input[i]>0?count=count+1:sum=sum+input[i]
+    }
+  }
+  return [count,sum]
+}
+
+// 8kyu: Abbreviate a Two Word Name
+function abbrevName(name){
+  return name.split(' ').map(x=>x[0].toUpperCase()).join('.')
+}
+
+// 8kyu: A Neelde in the Haystack
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`
+}
