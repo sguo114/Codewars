3134d52b58a6a142f252cd234914c28749bbb89b
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-04-27 15:23:01 -0400

    Update 2022_04_27.js

--- 2022_04_27.js
+++ 2022_04_27.js
@@ -1,5 +1,43 @@
 // 4 kata completed
 
+// 5kyu: String Incrementer
+function incrementString (strng) {
+  let num = []
+  let str = strng.split('')
+  while(!isNaN(str[str.length-1])){
+    num.push(str[str.length-1])
+    str.pop()
+  }
+  num.length>0? num = String(Number(num.reverse().join(''))+1).padStart(num.length,'0'): num = 1
+  return str.join('')+num
+}
+
+// 6kyu: Mexican Wave
+function wave(str){
+  let ans = []
+  if(str.length>1){
+    ans = str[0] !== ' '?[str[0].toUpperCase()+str.slice(1)]:[]
+    for(let i = 1; i<str.length;i++){
+      if(str[i]!= ' '){
+        ans.push(str.slice(0,i)+str[i].toUpperCase()+str.slice(i+1))
+      }
+    }
+  }
+  return ans
+}
+
+// 6kyu: Find the total white and black areas in a weird chessboard
+function whiteBlackAreas(cols, rows) {
+  let sumOfWhites = 0, sumOfBlacks = 0, N = cols.length
+  let evenColLength = 0, oddColLength = 0, evenRowLength = 0, oddRowLength = 0
+  for(let i = 0; i<N;i++){
+    i%2 == 0? (evenColLength += cols[i], evenRowLength += rows[i]):(oddColLength += cols[i], oddRowLength += rows[i])
+  }
+  sumOfWhites = evenColLength*evenRowLength + oddColLength*oddRowLength
+  sumOfBlacks = evenColLength*oddRowLength + oddColLength*evenRowLength
+  return [sumOfWhites, sumOfBlacks]
+}
+
 // 7kyu: Testing 1-2-3
 var number=function(array){
   return array.map((x,i)=>`${i+1}: ${x}`)
@@ -18,6 +56,12 @@ const sequenceSum = (begin, end, step) => {
   }
   return ans
 };
+
+// 8kyu: Grasshopper- Gradebook
+function getGrade (s1, s2, s3) {
+  let avg = (s1+s2+s3)/3 
+  return avg>=90? 'A': avg>=80? 'B': avg>=70? 'C': avg>=60? 'D':'F'
+}
 
 // 8kyu: You only need one- beginner
 function check(a, x) {
