2b7d1646416d31f3a48b7b08024d7aab21cced70
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-04-28 16:29:38 -0400

    Update 2022_04_28.js

--- 2022_04_28.js
+++ 2022_04_28.js
@@ -12,6 +12,22 @@ function bouncingBall(h,  bounce,  window) {
   } else{return -1}
 }
 
+// 7kyu: Sum of odd numbers
+function rowSumOddNumbers(n) {
+  let count = 0 
+  for(let i =1; i<n;i++){
+    count += i
+  }
+  
+  let ans = count*2-1
+  let arr = []
+  for(let i = 0; i<n; i++){
+    ans+=2
+    arr.push(ans)
+  }
+  return n==0||n==1? 1:arr.reduce((a,b)=>a+b)
+}
+
 // 7kyu: Factorial
 function factorial(n){
   let ans = n
