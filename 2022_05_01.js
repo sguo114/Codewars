5285c1fdbab42cd243bc26fca50d436e756832fa
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-02 12:32:19 -0400

    Update 2022_05_01.js

--- 2022_05_01.js
+++ 2022_05_01.js
@@ -1,4 +1,13 @@
-// 2 kata completed
+// 3 kata completed
+
+// 6kyu: Feynman's Square question
+function countSquares(n){
+  let ans = 0
+  for(let i = 1; i<=n;i++){
+    ans+= Math.pow(i,2)
+  }
+  return ans
+}
 
 // 8kyu: Beginner Series #2 Clock
 function past(h, m, s){
