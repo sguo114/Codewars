2be62d8f8d6f44614d8c19cfaadc3c59535a5fe0
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-02 12:22:16 -0400

    Create 2022_05_01.js

---
+++ 2022_05_01.js
@@ -0,0 +1,11 @@
+// 2 kata completed
+
+// 8kyu: Beginner Series #2 Clock
+function past(h, m, s){
+  return h*3600000 + m*60000 + s*1000
+}
+
+// 8kyu: Beginner Series #4 Cockroach
+function cockroachSpeed(s) {
+  return Math.floor(s*100000/3600)
+}
