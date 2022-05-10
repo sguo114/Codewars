a90614852642874b1960386173b5f08e197a1575
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-10 11:15:23 -0400

    Update 2022_05_10.js

--- 2022_05_10.js
+++ 2022_05_10.js
@@ -1,5 +1,27 @@
-// 2 kata completed
+// 3 kata completed
 
+// 4 kyu: Remove Zeros (no array or string methods allowed, no temporary arrays)
+function removeZeros(array) {
+  if(array.length<1){return array}
+  
+  let swap = true
+  
+  while(swap == true){
+    let count = 0
+    for(let i = 0; i< array.length-1; i++){
+      if((array[i] === 0 || array[i] ==='0') && (array[i+1]!=0||array[i+1] === false)){
+        let temp = array[i]
+        array[i] = array[i+1]
+        array[i+1] = temp
+      } else {
+        count += 1
+      }
+    }
+    count == array.length-1? swap = false: swap = true
+  }
+  
+  return array;
+}
 
 // 6kyu: Where is my parent?
 function findChildren(dancingBrigade) {
