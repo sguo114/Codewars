7a4264dd4c7505ea97471e707cbbb114716777ec
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-10 10:35:54 -0400

    Update 2022_05_10.js

--- 2022_05_10.js
+++ 2022_05_10.js
@@ -1,4 +1,19 @@
-// kata completed
+// 2 kata completed
+
+
+// 6kyu: Where is my parent?
+function findChildren(dancingBrigade) {
+  let peeps = dancingBrigade.split('').sort(), 
+      adults = peeps.filter(el=>el == el.toUpperCase()),
+      kids = peeps.filter(el=>el == el.toLowerCase()),
+      kidCount = {}
+  
+  for(let i = 0; i<kids.length;i++){
+    kidCount[kids[i]] ? kidCount[kids[i]] += 1 : kidCount[kids[i]] = 1
+  }
+  
+  return adults.map(el=>el + el.toLowerCase().repeat(kidCount[el.toLowerCase()])).join('');
+}
 
 // 6kyu: 1/n- Cycle
 function cycle(n) {
