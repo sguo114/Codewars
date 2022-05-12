c8257201063376e67a45b2eac8fc153bceb73172
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-12 10:17:29 -0400

    Update 2022_05_12.js

--- 2022_05_12.js
+++ 2022_05_12.js
@@ -1,4 +1,32 @@
-// kata completed
+// 3 kata completed
+
+// 6kyu: Mastermind Hint Peg
+function getHints(answer, guess) {
+  let ansFreq = {}, guessFreq = {}, black = 0, white = 0
+  while(guess.length>0){
+    if(answer[0]== guess[0]){
+      black += 1
+    } else{
+      ansFreq[answer[0]] ? ansFreq[answer[0]]+= 1 : ansFreq[answer[0]] = 1
+      guessFreq[guess[0]] ? guessFreq[guess[0]] += 1: guessFreq[guess[0]] = 1
+    }
+    answer.shift()
+    guess.shift()
+  }
+  
+  let keys = Object.keys(guessFreq)
+  for(let i = 0; i<keys.length; i++){
+    if(ansFreq[keys[i]]){
+      ansFreq[keys[i]] > guessFreq[keys[i]] ? white += guessFreq[keys[i]] : white += ansFreq[keys[i]]
+    }
+  }
+  return {'black':black, 'white':white}
+}
+
+// 7kyu: Summing a number's digits
+function sumDigits(number) {
+  return (''+Math.abs(number)).split('').reduce((a,b)=>a+Number(b),0)
+}
 
 // 7 kyu: Fix string case
 function solve(s){
