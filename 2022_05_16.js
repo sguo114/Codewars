fc4dc3d988b7d7543dddfc926f2dbf631d462617
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-16 15:59:57 -0400

    Update 2022_05_16.js

--- 2022_05_16.js
+++ 2022_05_16.js
@@ -1,4 +1,13 @@
-// kata completed
+// 2 kata completed
+
+// 5kyu: Sum of Pairs
+function sumPairs(ints, s) {
+  let seen = {};
+  for (let i=0; i < ints.length; i++) {
+    if (seen[s - ints[i]]) return [s-ints[i], ints[i]];
+    seen[ints[i]] = true;
+  }
+  }
 
 // 6kyu: Title Case
 function titleCase(title, minorWords) {
