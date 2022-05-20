e66190c44fe61c18b6b6fffb097a2d6b3444c33e
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-20 11:33:04 -0400

    Update 2022_05_20.js

--- 2022_05_20.js
+++ 2022_05_20.js
@@ -1,5 +1,17 @@
 // kata completed
 
+// 6kyu: Is a number prime?
+function isPrime(num) {
+  if (num < 2) return false;
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; ++i) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 // 6kyu: Sum of Parts
 function partsSums(ls) {
   let ans = [ls.reduce((a,b)=>a+b,0)]
