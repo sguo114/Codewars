b90b572342f7e3ea875d97af7164711fb2a1a050
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-23 12:30:09 -0400

    Update 2022_05_23.js

--- 2022_05_23.js
+++ 2022_05_23.js
@@ -1,4 +1,20 @@
-// kata completed
+// 2 kata completed
+
+// 6kyu:Your order, please
+function order(words){
+  let arr = words.split(' '), obj = {}
+  
+  for(let i = 0; i<arr.length; i++){
+    for(let j = 0; j<arr[i].length; j++){
+      if(!isNaN(arr[i][j])){
+        obj[arr[i][j]] = arr[i]
+      }
+    }
+  }
+  let ans = Object.keys(obj).sort((a,b)=>a-b).map(el=>obj[el])
+  
+  return ans.join(' ')
+}
 
 // 6 kyu: Run-length encoding
 var runLengthEncoding = function(str){
