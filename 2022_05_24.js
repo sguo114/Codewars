4f0b55841265dd258a9a2d0457c4bfa6714cb717
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-24 09:52:47 -0400

    Update 2022_05_24.js

--- 2022_05_24.js
+++ 2022_05_24.js
@@ -1,5 +1,17 @@
 // kata completed
 
+// 6 kyu: Matrix Transpose
+function transpose(matrix) {
+  let ans = []
+  for(let i = 0; i<matrix[0].length; i++){
+    ans.push([])
+    for(let j = 0; j<matrix.length; j++){
+      ans[i].push(matrix[j][i])
+    }
+  }
+  return ans
+}
+
 // 6kyu: Reach Me and Sum my Digits
 function sumDigNthTerm(initval, patternl, nthterm) {
   let sum = initval  
