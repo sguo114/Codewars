e89972386f34212ed3b9baaeb94b47242061cb95
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-28 21:40:29 -0400

    Create 2022_05_28.js

---
+++ 2022_05_28.js
@@ -0,0 +1,17 @@
+// kata completed
+
+// 6kyu: Pair of Gloves
+function numberOfPairs(gloves){
+  let pairs = {}, ans = 0
+  for(let i = 0; i<gloves.length;i++){
+    if(pairs[gloves[i]]){
+      if(pairs[gloves[i]]%2 == 1){
+        ans++
+      }
+      pairs[gloves[i]] += 1
+    } else{
+      pairs[gloves[i]] = 1
+    }
+  }
+  return ans
+}
