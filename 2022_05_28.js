0f8f87b71dd28e839c3ec349329fb49f52685ac8
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-05-28 21:51:13 -0400

    Update 2022_05_28.js

--- 2022_05_28.js
+++ 2022_05_28.js
@@ -15,3 +15,12 @@ function numberOfPairs(gloves){
   }
   return ans
 }
+
+// 8kyu: I love you a little, a lot, passionately,... not at all
+function howMuchILoveYou(nbPetals) {
+    return nbPetals%6 == 1 ? "I love you" :
+    nbPetals%6 == 2 ? "a little" :
+    nbPetals%6 == 3 ? "a lot" :
+    nbPetals%6 == 4 ? "passionately" :
+    nbPetals%6 == 5 ? "madly" : "not at all"
+}
