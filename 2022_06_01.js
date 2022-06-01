ca20ce600078e07afec8abfd7f8df329f81c5d78
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-06-01 08:42:45 -0400

    Create 2022_06_01.js

---
+++ 2022_06_01.js
@@ -0,0 +1,26 @@
+// kata completed
+
+// 6kyu: Help the bookseller!
+function stockList(listOfArt, listOfCat){
+  let freq = {}, ans = ""
+  
+  if(listOfArt <1 || listOfCat<1) return ""
+  
+  for(let i = 0; i< listOfArt.length; i++){
+    if(freq[listOfArt[i][0]]){
+      freq[listOfArt[i][0]] += Number(listOfArt[i].split(' ')[1])
+    } else{
+      freq[listOfArt[i][0]] = Number(listOfArt[i].split(' ')[1])
+    }
+  }
+  
+  for(let i = 0; i<listOfCat.length; i++){
+    let temp = `(${listOfCat[i]} : ${freq[listOfCat[i]] || 0})`
+    ans += temp
+    if(i<listOfCat.length-1){
+      ans+= ' - '
+    }
+  }
+  
+  return ans
+}
