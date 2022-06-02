07440ad631e2f9203359c5f4580ea724dd70bb2f
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-06-02 11:52:46 -0400

    Update 2022_06_02.js

--- 2022_06_02.js
+++ 2022_06_02.js
@@ -1,5 +1,19 @@
 // kata completed
 
+// 7kyu: Sum of two lowest possible integers
+function sumTwoSmallestNumbers(numbers) {  
+  let min1 = Math.min(numbers[0], numbers[1]), min2 = Math.max(numbers[0], numbers[1])
+  for(let i = 2; i<numbers.length; i++){
+    if(numbers[i] < min1){ 
+      min2 = min1
+      min1 = numbers[i]
+    } else if(numbers[i] < min2){
+      min2 = numbers[i]
+    }
+  }
+  return min1 + min2
+}
+
 // 7kyu: Sum of Minimums!
 function sumOfMinimums(arr) {
   let sum = 0; 
