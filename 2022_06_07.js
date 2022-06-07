a1b0666f07370134e749babc6f9b7ddc4d38b5bc
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-06-07 11:21:32 -0400

    Update 2022_06_07.js

--- 2022_06_07.js
+++ 2022_06_07.js
@@ -1,5 +1,30 @@
 // kata completed
 
+// 6kyu: Pascal's Triangle #2
+function pascal(depth) {
+  let arr = [[1], [1,1]]
+  
+  if(depth === 1) return [[1]]
+  if(depth === 2) return arr
+  
+  for(let i = 2; i<depth; i++){
+    arr.push([])
+    for(let j = 0; j<=i; j++ ){
+      if(j == 0) {
+        arr[i].push(1)
+        continue;
+      }
+      if(j == i) {
+        arr[i].push(1)
+        continue;
+      }
+      arr[i].push(arr[i-1][j-1] + arr[i-1][j])
+    }
+  }
+  
+  return arr
+}
+
 // 7kyu: Sorted? yes? no? how?
 function isSortedAndHow(array) {
   if(array[0]>array[1]){
