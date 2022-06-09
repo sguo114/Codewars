8c7f58d6e591ec6da82fbaf7ee4d052ff885040b
sguo114 <100171223+sguo114@users.noreply.github.com>
2022-06-09 11:34:05 -0400

    Update 2022_06_09.js

--- 2022_06_09.js
+++ 2022_06_09.js
@@ -1,4 +1,16 @@
-// kata completed
+// 2 kata completed
+
+// 6kyu: Good vs Evil
+function goodVsEvil(good, evil){
+  let gWorth = {0:1, 1:2, 2:3, 3:3, 4:4,5:10}, 
+      eWorth = {0:1,1:2,2:2,3:2,4:3,5:5,6:10},
+      gTot = good.split(' ').reduce((a,b,i) => a + b*gWorth[i], 0), 
+      eTot = evil.split(' ').reduce((a,b,i) => a+ b*eWorth[i], 0)
+  
+  return gTot > eTot ? 'Battle Result: Good triumphs over Evil':
+         eTot > gTot ? 'Battle Result: Evil eradicates all trace of Good' :
+         'Battle Result: No victor on this battle field'
+}
 
 // 8kyu: get character from ASCII Value
 function getChar(c){
