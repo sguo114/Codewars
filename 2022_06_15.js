99934e21913bde03e5d77f50224e5117c2d3e3f0
Samuel Guo <100171223+sguo114@users.noreply.github.com>
2022-06-15 09:24:42 -0400

    Update 2022_06_15.js

--- 2022_06_15.js
+++ 2022_06_15.js
@@ -2,11 +2,5 @@
 
 // 7kyu: sPoNgEbOb MeMe
 function spongeMeme(sentence) {
-  let sent = sentence.split('').map((el,i)=> {
-    if(/[a-zA-Z]/.test(el) && i%2 === 0) return el.toUpperCase();
-    else if(/[a-zA-Z]/.test(el) && i%2 !== 0) return el.toLowerCase();
-    else return el;
-  })
-  
-  return sent.join('')
+  return sentence.split('').map((el,i) => i%2 === 0 ? el.toUpperCase() : el.toLowerCase()).join('')
 }
