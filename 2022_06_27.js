c438613068310217d693fe59fb802bd2332d1b07
Samuel Guo <100171223+sguo114@users.noreply.github.com>
2022-06-27 10:41:18 -0400

    Update 2022_06_27.js

--- 2022_06_27.js
+++ 2022_06_27.js
@@ -1,5 +1,29 @@
 // kata completed
 
+// 6kyu: Ping Pong
+function pingPong(sounds) {
+  let pingPts = 0, pongPts = 0, last = "", first = "", bad = ""
+  
+  for(let i = 0; i<sounds.length; i+=5){
+    let sound = sounds.slice(i,i+4)
+    if(sound !== "ping" && sound !== "pong"){
+      if(first === "pong" && last === "ping"){
+        pongPts++;
+        first = ""
+      } else if(first === "ping" && last === "pong"){
+        pingPts++;
+        first = ""
+      }
+      bad = sound;
+    } else {
+      if(bad !== "ping" && bad!== "pong") first = sound
+      last = sound
+      bad = sound
+    }
+  }
+  return pingPts > pongPts ? "ping" : pingPts < pongPts ? "pong" : last === "ping" ? "pong" : "ping"
+}
+
 // 7kyu: Going to the cinema
 function movie(card, ticket, perc) {
   let count = 1, price = ticket*perc, B = card + price
