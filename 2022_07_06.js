62b10ae6092b5ba3267df55afc87a84a5570948f
Samuel Guo <100171223+sguo114@users.noreply.github.com>
2022-07-06 09:51:42 -0400

    Update 2022_07_06.js

--- 2022_07_06.js
+++ 2022_07_06.js
@@ -20,3 +20,13 @@ function findDiscounted(prices){
   
   return ans.join(' ')
 }
+
+// 6kyu: The Shell Game (cup swaps- find the ball given swaps)
+find_the_ball=function(start,swaps){
+  let last = start
+  for(let i = 0; i<swaps.length; i++){
+    if(swaps[i][0] == last) last = swaps[i][1]
+    else if(swaps[i][1] == last) last = swaps[i][0]
+  }
+  return last
+}
