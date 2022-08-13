3b4ffd4ecf11b5dc100aad7e4b9babb4cee11c9e
Samuel Guo <100171223+sguo114@users.noreply.github.com>
2022-08-13 11:12:38 -0400

    Update 2022_08_13.js

--- 2022_08_13.js
+++ 2022_08_13.js
@@ -1,5 +1,28 @@
 // kata completed
 
+// 6kyu: Arrh, grabscab! 
+function grabscrab(anagram, dictionary) {
+  let ans = []
+  for(let i = 0; i<dictionary.length; i++){
+    if(checkAna(anagram, dictionary[i])) ans.push(dictionary[i])
+  }
+  return ans
+}
+
+var checkAna = function(a, b){
+  if(a.length !== b.length) return false
+
+  let charMap = new Map()
+  for(const char of a){
+    charMap.set(char, (charMap.get(char) || 0) + 1)
+  }
+  for(const char of b){
+    if(!charMap.has(char) || charMap.get(char) === 0) return false
+    charMap.set(char, charMap.get(char) - 1)
+  }
+  return true
+}
+
 // 7kyu: Exes and Ohs
 function XO(str) {
   let x = 0, o = 0
